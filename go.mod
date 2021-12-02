7b9c7380b6c90e01bbc79ae198d2e66f0b1afdf2
David Nguyenn <thinhnv@boxme.asia>
2021-12-02 14:37:18 +0700

    refactor(all): add dotenv package, seperate production and testing mode

--- go.mod
+++ go.mod
@@ -11,6 +11,7 @@ require (
 	github.com/hashicorp/errwrap v1.1.0 // indirect
 	github.com/hashicorp/go-multierror v1.1.1 // indirect
 	github.com/jmoiron/sqlx v1.3.4
+	github.com/joho/godotenv v1.4.0 // indirect
 	github.com/lib/pq v1.10.4
 	github.com/oschwald/maxminddb-golang v1.8.0
 	github.com/stretchr/testify v1.7.0
--- main.go
+++ main.go
@@ -4,9 +4,11 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"net/url"
 	"os"
 
 	omisocial "github.com/Boxme-Global/tracking/src"
+	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
 
@@ -21,20 +23,56 @@ func Contains(s []string, str string) bool {
 }
 
 func main() {
+	omisocial.SetFingerprintKeys(42, 123)
+	err := godotenv.Load()
 
-	// Set default clickhouse uri
-	// os.Setenv("clickhouse", "10.148.0.23:9000")
-	clickhouseUri := os.Getenv("clickhouse")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	env := os.Getenv("ENV")
+	log.Println(env)
+	var (
+		migrate_uri string
+		client_uri  string
+	)
+
+	if env == "production" {
+		clickhouse_host := os.Getenv("CLICKHOUSE_HOST")
+		clickhouse_port := os.Getenv("CLICKHOUSE_PORT")
+
+		q := make(url.Values)
+		q.Set("username", os.Getenv("CLICKHOUSE_USERNAME"))
+		q.Set("password", os.Getenv("CLICKHOUSE_PASSWORD"))
+		q.Set("database", os.Getenv("CLICKHOUSE_DATABASE"))
+
+		client_uri = (&url.URL{
+			Scheme:   "tcp",
+			Host:     clickhouse_host + ":" + clickhouse_port,
+			RawQuery: q.Encode(),
+		}).String()
+
+		q.Set("x-multi-statement", "true")
+
+		migrate_uri = (&url.URL{
+			Scheme:   "clickhouse",
+			Host:     clickhouse_host + ":" + clickhouse_port,
+			RawQuery: q.Encode(),
+		}).String()
+
+	} else {
+		migrate_uri = "clickhouse://10.148.0.23:9000?x-multi-statement=true"
+		client_uri = "tcp://10.148.0.23:9000"
+
+	}
 
 	// Set the key for SipHash. This should be called on startup (before generating the first fingerprint) and is NOT concurrency save.
-	omisocial.SetFingerprintKeys(42, 123)
 
 	// Migrate the database.
-	omisocial.Migrate("clickhouse://" + clickhouseUri + "?x-multi-statement=true")
+	omisocial.Migrate(migrate_uri)
 
 	// Create a new ClickHouse client to save hits.
-	store, _ := omisocial.NewClient("tcp://"+clickhouseUri, nil)
-
+	store, _ := omisocial.NewClient(client_uri, nil)
 	// Set up a default tracker with a salt.
 	// This will buffer and store hits and generate sessions by default.
 	tracker := omisocial.NewTracker(store, "BuS7BsvURhatRPqr", nil)
